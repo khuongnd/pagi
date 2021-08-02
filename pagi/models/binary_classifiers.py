e08b77d4752091df9299cd5bba4c71b21bf82da9
Khuong Nguyen <khuongnd@gmail.com>
2021-08-02 15:38:45 +0700

    Add classifier

---
+++ pagi/models/binary_classifiers.py
@@ -0,0 +1,45 @@
+from catboost import CatBoostClassifier
+
+
+def get_CatBoostClassifier(iterations=1000):
+    return CatBoostClassifier(
+        iterations=iterations,
+        learning_rate=0.01,
+        min_data_in_leaf=10,
+        eval_metric='AUC'
+    )
+
+
+if __name__ == "__main__":
+    from catboost.datasets import titanic
+    import numpy as np
+    train_df, test_df = titanic()
+
+    train_df.head()
+    train_df.fillna(-999, inplace=True)
+    test_df.fillna(-999, inplace=True)
+    X = train_df.drop('Survived', axis=1)
+    y = train_df.Survived
+    print(X.dtypes)
+
+    categorical_features_indices = np.where(X.dtypes != np.float)[0]
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, train_size=0.75, random_state=42)
+
+    X_test = test_df
+
+    model = get_CatBoostClassifier()
+
+    model.fit(
+        X_train, y_train,
+        cat_features=categorical_features_indices,
+        eval_set=(X_validation, y_validation),
+        plot=True
+    )
+
+    preds = model.predict_proba(X_test)[:, 1]
+
+    from pagi.utils.metrics import get_binary_classification_metrics
+
+    print(get_binary_classification_metrics(y_true, preds))
