5705c89f06c2872bf25772ea45d54cebdd9925f9
Khuong Nguyen <khuongnd@gmail.com>
2021-08-02 16:21:51 +0700

    Add metrics

--- pagi/utils/metrics.py
+++ pagi/utils/metrics.py
@@ -1,12 +1,15 @@
 from sklearn.metrics import *
+import numpy as np
 
 
 def get_binary_classification_metrics(y_true, y_pred):
+    y_pred = np.asarray(y_pred)
+    y_pred_int = np.where(y_pred >= 0.5, 1, 0)
     return {
-        "accuracy": accuracy_score(y_true, y_pred),
-        "precision": precision_score(y_true, y_pred),
-        "recall": recall_score(y_true, y_pred),
+        "accuracy": accuracy_score(y_true, y_pred_int),
+        "precision": precision_score(y_true, y_pred_int),
+        "recall": recall_score(y_true, y_pred_int),
         "roc_auc": roc_auc_score(y_true, y_pred),
-        "f1": f1_score(y_true, y_pred),
-        "mcc": matthews_corrcoef(y_true, y_pred)
+        "f1": f1_score(y_true, y_pred_int),
+        "mcc": matthews_corrcoef(y_true, y_pred_int)
     }
