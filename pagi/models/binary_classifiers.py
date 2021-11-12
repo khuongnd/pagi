9ee639ff1f7488a9d3e1c3439fdea53edb4a379b
Khuong Nguyen <87292186+khuongnd6@users.noreply.github.com>
2021-11-13 05:46:23 +0700

    add ViT models

--- pagi/models/binary_classifiers.py
+++ pagi/models/binary_classifiers.py
@@ -10,39 +10,3 @@ def get_CatBoostClassifier(iterations=1000, learning_rate=0.01, min_data_in_leaf
         cat_features=cat_features
     )
 
-
-if __name__ == "__main__":
-    from catboost.datasets import titanic
-    import numpy as np
-
-    train_df, test_df = titanic()
-
-    train_df.head()
-    train_df.fillna(-999, inplace=True)
-    test_df.fillna(-999, inplace=True)
-    X = train_df.drop('Survived', axis=1)
-    y = train_df.Survived
-    print(X.dtypes)
-
-    categorical_features_indices = np.where(X.dtypes != np.float)[0]
-    from sklearn.model_selection import train_test_split
-
-    X_train, X_validation, y_train, y_validation = train_test_split(X, y, train_size=0.75, random_state=42)
-
-    X_test = test_df
-
-    model = get_CatBoostClassifier()
-
-    model.fit(
-        X_train, y_train,
-        cat_features=categorical_features_indices,
-        eval_set=(X_validation, y_validation),
-        plot=True
-    )
-
-    preds = model.predict_proba(X_validation)[:, 1]
-
-    from pagi.utils.metrics import get_binary_classification_metrics
-
-    print(y_validation.values.shape, preds.shape)
-    print(get_binary_classification_metrics(y_validation.values.astype(np.float), preds))
