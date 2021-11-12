b8cdfecdaa5fa37f95e97512ce4616b00b027534
Khuong Nguyen <87292186+khuongnd6@users.noreply.github.com>
2021-11-13 05:07:07 +0700

    Add classifier

--- pagi/models/binary_classifiers.py
+++ pagi/models/binary_classifiers.py
@@ -1,12 +1,12 @@
 from catboost import CatBoostClassifier
 
 
-def get_CatBoostClassifier(iterations=1000, min_data_in_leaf=30, cat_features=None):
+def get_CatBoostClassifier(iterations=1000, learning_rate=0.01, min_data_in_leaf=30, eval_metric='AUC', cat_features=None):
     return CatBoostClassifier(
         iterations=iterations,
         learning_rate=0.01,
         min_data_in_leaf=min_data_in_leaf,
-        eval_metric='AUC',
+        eval_metric=eval_metric,
         cat_features=cat_features
     )
 
